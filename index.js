6569223eb6b891d7c963ff98418c66359cb93f25
+ <+>
2022-06-29 10:57:49 -0600

    commit tree 0d157aa87b468e60617dd646183c6b0024f3b81c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// let findYear = record.find(superbowlWin);
+
+function superbowlWin(collection){
+    for (let values of collection){
+        if(values.result=== 'W'){
+            return values.year;
+        }
+        // else{
+        //     return('Sad reality');
+        // }
+    }
+
+}
+
+record.find(superbowlWin);
+
+
